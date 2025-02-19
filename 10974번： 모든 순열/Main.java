5abae359390d5e3a03c988861d8388c218cc1771
SoTaeHo <sth9057@gmail.com>
2025-02-19 11:26:22 +0900

    BOJ-EX: 2/19/2025, 11:26:21 AM

--- 10974번： 모든 순열/Main.java
+++ 10974번： 모든 순열/Main.java
@@ -10,80 +10,40 @@
 // /*                                                                            */
 // /* ************************************************************************** */
 
-// import java.util.Scanner;
-
-// public class Main {
-
-//     static int n;
-//     static boolean[] visited;
-//     static int[] arr;
-
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         n = sc.nextInt();
-//         visited = new boolean[n];
-//         arr = new int[n];
-//         find(0);
-//         sc.close();
-//     }
-
-//     public static void find(int cnt) {
-//         if (cnt == n) {
-//             StringBuilder sb = new StringBuilder("");
-//             for (int i = 0; i < n; i++) {
-//                 sb.append(arr[i]).append(" ");
-//             }
-//             sb.append('\n');
-//             System.out.print(sb);
-//             return;
-//         }
-//         for (int i = 0; i < n; i++) {
-//             if (visited[i])
-//                 continue;
-//             visited[i] = true;
-//             arr[cnt] = i + 1;
-//             find(cnt + 1);
-//             visited[i] = false;
-//         }
-//     }
-// }
-
-import java.io.IOException;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    private static int N;
-    private static char[] ans;
-    private static boolean[] visit;
-    private static StringBuilder sb;
 
-    private static void backTracking(int len) {
+    static int n;
+    static boolean[] visited;
+    static int[] arr;
 
-        if (len == N) {
-            sb.append(ans);
-            return;
-        }
-
-        for (int i = 1; i <= N; i++) {
-            if (visit[i])
-                continue;
-            ans[len << 1] = (char) (i + '0');
-            visit[i] = true;
-            backTracking(len + 1);
-            visit[i] = false;
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        visited = new boolean[n];
+        arr = new int[n];
+        find(0);
+        sc.close();
     }
 
-    public static void main(String[] args) throws IOException {
-        N = System.in.read() - '0';
-        sb = new StringBuilder();
-        ans = new char[N << 1];
-        visit = new boolean[N + 1];
-        Arrays.fill(ans, ' ');
-        ans[(N << 1) - 1] = '\n';
-
-        backTracking(0);
-
-        System.out.println(sb);
+    public static void find(int cnt) {
+        if (cnt == n) {
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < n; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append('\n');
+            System.out.print(sb);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (visited[i])
+                continue;
+            visited[i] = true;
+            arr[cnt] = i + 1;
+            find(cnt + 1);
+            visited[i] = false;
+        }
     }
 }
