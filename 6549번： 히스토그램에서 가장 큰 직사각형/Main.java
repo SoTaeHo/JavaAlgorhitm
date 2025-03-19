db89e7708ba924e25c5b3badf571afd4ca1e3fe9
SoTaeHo <sth9057@gmail.com>
2025-03-19 17:48:38 +0900

    6549 시작

---
+++ 6549번： 히스토그램에서 가장 큰 직사각형/Main.java
@@ -0,0 +1,22 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/* ::: ::: ::: */
+/* Problem Number: 6549 :+: :+: :+: */
+/* +:+ +:+ +:+ */
+/* By: thxogh1 <boj.kr/u/thxogh1> +#+ +#+ +#+ */
+/* +#+ +#+ +#+ */
+/* https://boj.kr/6549 #+# #+# #+# */
+/* Solved: 2025/03/19 17:48:05 by thxogh1 ### ### ##.kr */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
