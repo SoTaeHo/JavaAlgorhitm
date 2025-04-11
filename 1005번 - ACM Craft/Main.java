05a1a8f97e5811c4cd968c7fd10147dbb25576bf
소태호 <sth9057@gmail.com>
2025-04-11 20:02:49 +0900

    위상정렬 / 초기화 및 큐에 넣는 경우를 잘못 사용하여 메모리 초과가 났었음. 정신차리고 잘 짜자자

--- 1005번 - ACM Craft/Main.java
+++ 1005번 - ACM Craft/Main.java
@@ -12,7 +12,10 @@
 /* ************************************************************************** */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,50 +23,54 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
+        StringBuilder sb = new StringBuilder();
         int tc = Integer.parseInt(br.readLine());
 
         for (int t = 0; t < tc; t++) {
-            int[][] map = new int[1001][1001];
-            int[] count = new int[1001];
 
             st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
 
-            int[] weight = new int[1001];
+            int[] degree = new int[n + 1];
+            int[] weight = new int[n + 1];
+            int[] ans = new int[n + 1];
             for (int i = 1; i < n + 1; i++) {
                 weight[i] = Integer.parseInt(st.nextToken());
+            }
+            List<List<Integer>> arr = new ArrayList<>();
+            for (int i = 0; i < n + 1; i++) {
+                arr.add(new ArrayList<>());
             }
             for (int i = 0; i < k; i++) {
                 st = new StringTokenizer(br.readLine());
                 int s = Integer.parseInt(st.nextToken());
                 int e = Integer.parseInt(st.nextToken());
-                map[s][e] = 1;
-                count[e] += 1;
+                arr.get(s).add(e);
+                degree[e] += 1;
             }
             int w = Integer.parseInt(br.readLine());
+            Queue<Integer> q = new ArrayDeque<>();
+            for (int i = 1; i < n + 1; i++) {
+                ans[i] = weight[i];
+                if (degree[i] == 0) {
+                    q.offer(i);
+                }
+            }
+            while (!q.isEmpty()) {
+                int node = q.poll();
+                for (int i = 0; i < arr.get(node).size(); i++) {
+                    int num = arr.get(node).get(i);
+                    degree[num] -= 1;
+                    ans[num] = Math.max(weight[num] + ans[node], ans[num]);
+                    if (degree[num] == 0)
+                        q.offer(num);
+
+                }
+            }
+            sb.append(ans[w]).append('\n');
         }
-    }
-
-    static class Node {
-        int index;
-        int weight;
-
-        public Node(int index, int weight) {
-            this.index = index;
-            this.weight = weight;
-        }
-    }
-
-    static class Edge {
-        int start;
-        int end;
-
-        public Edge(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
+        System.out.println(sb);
     }
 }
