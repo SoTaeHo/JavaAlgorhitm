ee7d13ff4c64037fb17d6706a31239533766032c
taeho so <91146046+SoTaeHo@users.noreply.github.com>
2025-04-21 13:46:44 +0900

    deque /

---
+++ 2164번 - 카드2/Main.java
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2164                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: thxogh1 <boj.kr/u/thxogh1>                  +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2164                           #+#        #+#      #+#    */
+/*   Solved: 2025/04/21 13:41:28 by thxogh1       ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+
+        for (int i = 1; i < n + 1; i++) {
+            dq.offer(i);
+        }
+
+        while (dq.size() != 1) {
+            dq.pollFirst();
+            dq.addLast(dq.pollFirst());
+        }
+        System.out.println(dq.poll());
+    }
+}
