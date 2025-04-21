388355435af73c24cc5456b936bcb4f8fab416ea
taeho so <91146046+SoTaeHo@users.noreply.github.com>
2025-04-22 08:29:10 +0900

    그리디 /

---
+++ 11047번 - 동전 0/Main.java
@@ -0,0 +1,40 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 11047                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: thxogh1 <boj.kr/u/thxogh1>                  +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/11047                          #+#        #+#      #+#    */
+/*   Solved: 2025/04/22 08:23:22 by thxogh1       ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[n];
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            ans += k / coin[i];
+            k %= coin[i];
+            if (k == 0) {
+                break;
+            }
+        }
+        System.out.println(ans);
+    }
+}
