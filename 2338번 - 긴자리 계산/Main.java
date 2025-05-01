d85b804fe285235ef68902f1005786e0c7052bf5
SoTaeHo <91146046+SoTaeHo@users.noreply.github.com>
2025-05-01 23:04:51 +0900

    브론즈

---
+++ 2338번 - 긴자리 계산/Main.java
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2338                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: thxogh1 <boj.kr/u/thxogh1>                  +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2338                           #+#        #+#      #+#    */
+/*   Solved: 2025/05/01 22:59:11 by thxogh1       ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger a = new BigInteger(br.readLine());
+        BigInteger b = new BigInteger(br.readLine());
+
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.println(a.multiply(b));
+    }
+}
