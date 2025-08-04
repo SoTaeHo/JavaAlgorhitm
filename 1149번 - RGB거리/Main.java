d36fa62682baab57611ef867406c0141af6492c1
소태호 <sth9057@gmail.com>
2025-08-05 08:18:43 +0900

    dp / RGB거리 2(17404)를 풀었어서 쉽게 해결

---
+++ 1149번 - RGB거리/Main.java
@@ -0,0 +1,45 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 1149                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: thxogh1 <boj.kr/u/thxogh1>                  +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/1149                           #+#        #+#      #+#    */
+/*   Solved: 2025/08/05 08:00:57 by thxogh1       ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][3];
+        int[][] dp = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = arr[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] = arr[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][2] = arr[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+        }
+
+        System.out.println(Math.min(Math.min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]));
+    }
+}
