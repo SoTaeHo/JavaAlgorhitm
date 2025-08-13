b5f0ff2cf570a2d18574f8943a298b5ba5fd56f9
소태호 <sth9057@gmail.com>
2025-08-13 21:14:17 +0900

    리스트 / 일단 오늘 하루를 넘기자

---
+++ 11651번 - 좌표 정렬하기 2/Main.java
@@ -0,0 +1,61 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 11651                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: thxogh1 <boj.kr/u/thxogh1>                  +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/11651                          #+#        #+#      #+#    */
+/*   Solved: 2025/08/13 21:02:38 by thxogh1       ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class Point implements Comparable<Point> {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Main.Point o) {
+
+            if (this.y != o.y) {
+                return Integer.compare(this.y, o.y);
+            } else {
+                return Integer.compare(this.x, o.x);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        List<Point> li = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            li.add(new Point(x, y));
+        }
+
+        Collections.sort(li);
+
+        for (Point p : li) {
+            System.out.println(p.x + " " + p.y);
+        }
+    }
+}
---
+++ 1193번 - 분수찾기/Main.java
@@ -0,0 +1,23 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 1193                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: thxogh1 <boj.kr/u/thxogh1>                  +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/1193                           #+#        #+#      #+#    */
+/*   Solved: 2025/08/13 20:30:00 by thxogh1       ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+    }
+}
