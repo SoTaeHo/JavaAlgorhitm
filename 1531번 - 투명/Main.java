bcc138a88aa54ce5f6f4bcd73ca2708ee5470e7b
소태호 <sth9057@gmail.com>
2025-09-25 17:21:24 +0900

    구현 / 간단한 for문 문제

---
+++ 1531번 - 투명/Main.java
@@ -0,0 +1,52 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 1531                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: thxogh1 <boj.kr/u/thxogh1>                  +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/1531                           #+#        #+#      #+#    */
+/*   Solved: 2025/09/25 17:11:41 by thxogh1       ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[101][101]
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            for (int j = x1; j <= x2; j++) {
+                for (int k = y1; k <= y2; k++) {
+                    arr[j][k] += 1;
+                }
+            }
+        }
+        int ans = 0;
+
+        for (int i = 1; i < 101; i++) {
+            for (int j = 1; j < 101; j++) {
+                if (arr[i][j] > m) {
+                    ans += 1;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
