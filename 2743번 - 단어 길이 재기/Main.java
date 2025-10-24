3add266b0634250beaa00c636713393b51088f5d
소태호 <sth9057@gmail.com>
2025-10-24 17:49:09 +0900

    브론즈 / 쉬어가기

---
+++ 2743번 - 단어 길이 재기/Main.java
@@ -0,0 +1,22 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2743                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: thxogh1 <boj.kr/u/thxogh1>                  +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2743                           #+#        #+#      #+#    */
+/*   Solved: 2025/10/24 17:46:30 by thxogh1       ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+`
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+}
