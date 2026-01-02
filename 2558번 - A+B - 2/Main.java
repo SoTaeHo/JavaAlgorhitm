8b91c09deed8922ff626397b806ac8ba73b4f3de
SoTaeHo <91146046+SoTaeHo@users.noreply.github.com>
2026-01-02 21:22:22 +0900

    쉬어가기

---
+++ 2558번 - A+B - 2/Main.java
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2558                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: thxogh1 <boj.kr/u/thxogh1>                  +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2558                           #+#        #+#      #+#    */
+/*   Solved: 2026/01/02 21:20:18 by thxogh1       ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(a + b);
+    }
+}
