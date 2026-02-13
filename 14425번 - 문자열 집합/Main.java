580c21497561e8101fd22d1bb2aa9706fe2ac7e3
소태호 <sth9057@gmail.com>
2026-02-13 12:13:33 +0900

    문자열, 트리 / 브루트 포스했다가 당황쓰

---
+++ 14425번 - 문자열 집합/Main.java
@@ -0,0 +1,42 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 14425                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: thxogh1 <boj.kr/u/thxogh1>                  +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/14425                          #+#        #+#      #+#    */
+/*   Solved: 2026/02/13 11:56:03 by thxogh1       ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Set<String> S = new HashSet<>();
+        String[] arr = new String[M];
+        for (int i = 0; i < N; i++) {
+            S.add(br.readLine());
+        }
+        int ans = 0;
+        for (int i = 0; i < M; i++) {
+            String str = br.readLine();
+            if (S.contains(str)) {
+                ans += 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
