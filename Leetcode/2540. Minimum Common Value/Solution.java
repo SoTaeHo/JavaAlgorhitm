30662e9239c907bd5c9f2577b733d4c3a8eba9a9
소태호 <sth9057@gmail.com>
2026-05-19 17:19:10 +0900

    daily / 조기 종료 조건 생각해보기

---
+++ Leetcode/2540. Minimum Common Value/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+
+        int idx1 = 0, idx2 = 0;
+
+        while (idx1 < nums1.length && idx2 < nums2.length) {
+            if (nums1[idx1] == nums2[idx2]) {
+                return nums1[idx1];
+            } else if (nums1[idx1] > nums2[idx2]) {
+                idx2 += 1;
+            } else {
+                idx1 += 1;
+            }
+        }
+
+        return -1;
+    }
+}
