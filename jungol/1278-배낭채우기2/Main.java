33d470ebc177324548e563368d2a475b5e289894
소태호 <sth9057@gmail.com>
2026-06-04 17:07:07 +0900

    DP / 베이직한 DP

---
+++ jungol/1278-배낭채우기2/Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+
+        int[] weight = new int[N];
+        int[] value = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            weight[i] = Integer.parseInt(st.nextToken());
+            value[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[W + 1];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = W; j >= weight[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
+            }
+        }
+        System.out.println(dp[W]);
+    }
+}
